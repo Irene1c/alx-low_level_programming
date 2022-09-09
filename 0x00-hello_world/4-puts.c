f0979c4ef9d02d81a9a81258048f02289c713742
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-09-08 17:33:33 -0700

    Use of puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C program to show the use of puts'
+ *
+ * Return: zero (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

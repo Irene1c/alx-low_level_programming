836fec57c78caa469b67df292cd04bc0914fd3a9
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-09-10 09:53:36 -0700

    Combination of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,13 @@ int main(void)
 	int i = '0';
 
 	for (i = '0'; i <= '9'; i++)
-
-	if (i == '9')
 	{
 		putchar(i);
-	}
-	else if (i <= '9')
-	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	{

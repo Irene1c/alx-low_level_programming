f899734f83b7349d338b8f5fd7d7d733da94193c
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-09-12 07:00:43 -0700

    Lowercase alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'To print alphabets in lower case using putchar'
+ * Description: 'To print lower case alphabets using putchar'
  *
  * Return:Always zero(0)
  */

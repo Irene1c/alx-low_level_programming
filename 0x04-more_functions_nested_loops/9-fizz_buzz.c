73ff23fa61a0e92d2d5e3c97ce20ab85107d3d7a
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-09-16 13:49:56 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,24 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0)
+		{
 			printf("Fizz ");
+		}
 		else if ((i % 5) == 0)
+		{
 			printf("Buzz ");
+		}
 		else if ((i % 15) == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
-	printf("\n");
+	{
+		printf("\n");
+	}
 	return (0);
 }

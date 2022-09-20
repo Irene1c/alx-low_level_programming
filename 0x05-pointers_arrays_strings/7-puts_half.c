4ab507027bdeb0ff90a36c79dc06292373672c7e
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-09-20 14:02:53 -0700

    Print half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 	len = _strlen(str);
 	if ((len % 2) == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -23,7 +23,7 @@ void puts_half(char *str)
 	else
 	{
 		n = (len - 1) / 2;
-		for (i = n; i < len; i++)
+		for (i = n; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}

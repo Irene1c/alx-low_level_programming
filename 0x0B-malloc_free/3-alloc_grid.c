a619cfc057f87a52063ee9042ed92117094fc11b
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-06 02:14:29 -0700

    2-dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,6 +21,7 @@ int **alloc_grid(int width, int height)
 	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 	{
+		free(arr);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -28,6 +29,7 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
+			free(arr[i]);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)

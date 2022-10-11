e55ea20b74032ae41cd791e7cb8af47d13ea9c7b
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-11 01:34:32 -0700

    more malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _strlen(char *s);
-char *argstostr(int ac, char **av);
 
 #endif

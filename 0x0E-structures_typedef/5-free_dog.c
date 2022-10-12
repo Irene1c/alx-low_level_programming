a2bf746943997e8d46dd7dcbc813dbe2269ba13d
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-12 09:51:41 -0700

    Frees dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: Pointer to variable to be freed
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

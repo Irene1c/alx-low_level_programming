86c5bc2b056867a607a394d56039a7b37cd5c8c7
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-28 07:08:06 -0700

    free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = NULL;
+	listint_t *Headrep;
+	listint_t *temp;
 
-	if (*head == NULL || head == NULL)
+	if (head != NULL)
 	{
-		return;
-	}
-	while (*head != NULL)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		Headrep = *head;
+		while (Headrep != NULL)
+		{
+			temp = Headrep;
+			Headrep = Headrep->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
 }

b4b79ed91aa9cb9619bd9221cfec229e431a7251
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-02 07:25:34 -0700

    convert binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,10 +10,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int value = 0;
 	int i = 0;
 	int base = 1;
-	int len = 0;
-	int j;
 
-	len = _strlen(b);
 	if (b == NULL)
 	{
 		return (0);
@@ -26,9 +23,9 @@ unsigned int binary_to_uint(const char *b)
 		}
 		i++;
 	}
-	for (j = len - 1; j >= 0; j--)
+	for (; i >= 0; i--)
 	{
-		if (b[j] == '1')
+		if (b[i] == '1')
 		{
 			value += base;
 		}
@@ -36,19 +33,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (value);
 }
-/**
- * _strlen - length of string
- * @s: the string
- * Return: length
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-

6966b52c5cc5e1100885bf74166208300c6dad58
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-01 18:37:25 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int set_bits_count(unsigned long int a);
+
+#endif

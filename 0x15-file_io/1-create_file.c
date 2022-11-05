4b46807fa5302c28c9c4bac4eb01db6f69073889
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-05 10:14:09 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int len;
 	int bytes;
 
-	len = _strlen(text_content) + 1;
+	len = _strlen(text_content);
 	if (filename == NULL)
 	{
 		return (-1);
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 	{
 		return (-1);

ebc71ebc4fef5c670ef131632340159c577c1b67
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-05 10:17:46 -0700

    update create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 	{
 		return (-1);

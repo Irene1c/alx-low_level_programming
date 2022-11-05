2464fa3b16ddc8910f08e596b696c151ba28bc74
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-05 09:57:25 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: string to write
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+	int bytes;
+
+	len = _strlen(text_content) + 1;
+	if (filename == NULL || text_content == 0)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	bytes = write(fd, text_content, len);
+	if (bytes == -1 || bytes != len)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - length of string
+ * @s: the string
+ * Return: length
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}

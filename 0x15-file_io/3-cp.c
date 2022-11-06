80977ababfc8196165ff6f36da77a1bd55e8a26e
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-11-06 07:00:52 -0800

    copy file content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,6 +74,11 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{

8866e29cc2d1c6e8e3a84fe8e7e9692618046da7
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-12-16 00:12:30 -0800

    fix new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 	if (idx == 0)
 	{
 		new->next = *h;

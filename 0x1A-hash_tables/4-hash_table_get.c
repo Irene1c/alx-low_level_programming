9a66f1c868c36ef026e568bcfc7fca2175206d0a
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-13 07:35:14 -0800

    retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *item;
 
+	if (key == NULL)
+	{
+		return (NULL);
+	}
+
 	index = key_index((unsigned char *)key, ht->size);
 	item = ht->array[index];
 

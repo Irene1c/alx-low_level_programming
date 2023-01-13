f24c6842319db622c92cde4859722c5750c7b4f0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-13 04:32:03 -0800

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,32 +18,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || *key == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	new_item = malloc(sizeof(hash_node_t *));
-	if (new_item == NULL)
-	{
-		return (0);
-	}
 	item = ht->array[index];
 	if (item == NULL)
 	{
+		new_item = malloc(sizeof(hash_node_t *));
+		if (new_item == NULL)
+		{
+			return (0);
+		}
 		new_item->key = strdup(key);
 		new_item->value = strdup(value);
 		item = new_item;
-		return (1);
 	}
 	else if (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 			item->value = strdup(value);
-		return (1);
 	}
 	else
 	{
+		new_item = malloc(sizeof(hash_node_t *));
+		if (new_item == NULL)
+		{
+			return (0);
+		}
 		new_item->key = strdup(key);
 		new_item->value = strdup(value);
 		new_item->next = item;
 		item = new_item;
-		return (1);
 	}
 	return (1);
 }

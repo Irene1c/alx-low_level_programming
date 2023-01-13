c53ced3d8d286544f8aff8193353c1bff167c10f
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-13 06:52:30 -0800

    fixes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,41 +11,31 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_item, *item;
-	unsigned int index;
+	unsigned long int index = 0;
 
 	if (ht == NULL)
 		return (0);
 	if (key == NULL || *key == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	new_item = malloc(sizeof(hash_node_t));
-	if (new_item == NULL)
-	{
-		return (0);
-	}
 	item = ht->array[index];
-	if (item == NULL)
-	{
-		new_item->key = strdup(key);
-		new_item->value = strdup(value);
-		item = new_item;
-		return (1);
-	}
-	else if (item != NULL)
+	if (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 		{
 			item->value = strdup(value);
 			return (1);
 		}
+		item = item->next;
 	}
-	else
+	new_item = malloc(sizeof(hash_node_t));
+	if (new_item == NULL)
 	{
-		new_item->key = strdup(key);
-		new_item->value = strdup(value);
-		new_item->next = item;
-		item = new_item;
-		return (1);
+		return (0);
 	}
+	new_item->key = strdup(key);
+	new_item->value = strdup(value);
+	new_item->next = ht->array[index];
+	ht->array[index] = new_item;
 	return (1);
 }

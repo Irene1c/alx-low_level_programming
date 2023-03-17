235204613dc827ea5df8925c0ed414dbb11879c7
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-03-17 11:48:33 -0700

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,6 +20,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		free(temp);
+		temp = *head;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		return (1);
 	}
 	for (i = 0; temp != NULL && i < index; i++)

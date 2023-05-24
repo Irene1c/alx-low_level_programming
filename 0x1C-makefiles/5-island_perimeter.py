be178a7f1990388128f94a9aeec0dbc19c9aaf2b
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-05-24 11:05:54 -0700

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" island perimeter """
+
+
+def island_perimeter(grid):
+    """  returns the perimeter of the island described in grid"""
+
+    c = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j - 1] == 0:
+                    c += 1
+                if grid[i][j + 1] == 0:
+                    c += 1
+                if grid[i - 1][j] == 0:
+                    c += 1
+                if grid[i + 1][j] == 0:
+                    c += 1
+    return c

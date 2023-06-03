b9c64b6b7f6394f4a49cfbcde65584b8e35c1fbb
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-03 02:20:06 -0700

    update Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,16 @@ def island_perimeter(grid):
     c = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
-                    c += 1
-                if grid[i][j + 1] == 0:
-                    c += 1
-                if grid[i - 1][j] == 0:
-                    c += 1
-                if grid[i + 1][j] == 0:
-                    c += 1
+            try:
+                if grid[i][j] == 1:
+                    if grid[i][j - 1] == 0:
+                        c += 1
+                    if grid[i][j + 1] == 0:
+                        c += 1
+                    if grid[i - 1][j] == 0:
+                        c += 1
+                    if grid[i + 1][j] == 0:
+                        c += 1
+            except IndeexError:
+                c += 1
     return c

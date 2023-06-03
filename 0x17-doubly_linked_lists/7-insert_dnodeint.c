8fa6f7b8742d78c2fd79d93f5a542108c0a46311
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-03 04:15:01 -0700

    update Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,8 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new;
+		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}

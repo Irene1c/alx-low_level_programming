6107194c8d05b5d300ad9c0f66bd249f44e7a1c9
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-28 14:59:44 -0800

    Remove try/except block to Handle IndexError explicitly and checking validity of the indices before accessing them

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,13 @@ def island_perimeter(grid):
     c = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            try:
-                if grid[i][j] == 1:
-                    if grid[i][j - 1] == 0:
-                        c += 1
-                    if grid[i][j + 1] == 0:
-                        c += 1
-                    if grid[i - 1][j] == 0:
-                        c += 1
-                    if grid[i + 1][j] == 0:
-                        c += 1
-            except IndexError:
-                c += 1
+            if grid[i][j] == 1:
+                if grid[i][j - 1] == 0 or j == 0:
+                    c += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    c += 1
+                if grid[i - 1][j] == 0 or i == 0:
+                    c += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    c += 1
     return c
